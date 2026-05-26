6af7fceeb81359de3072ce2be2ef5ee11f17be14
Angel <angelica.oliveira@dcx.ufpb.br>
2026-05-25 23:27:34 -0300

    Passo 1 do exercicio 2

--- src/main/java/br/ufpb/dcx/angelica/Livro.java
+++ src/main/java/br/ufpb/dcx/angelica/Livro.java
@@ -1,5 +1,66 @@
 package br.ufpb.dcx.angelica;
 
+import java.util.Objects;
+
+//atributos
 public class Livro {
+    private String titulo;
+    private String autor;
+    private int quantPaginas;
+    private double preco;
+
+    //construtores
+    public Livro(String titulo, String autor, int quantPaginas, double preco){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.quantPaginas = quantPaginas;
+        this.preco = preco;
+    }
+
+    //get e set
+    public String getTitulo(){
+        return titulo;
+    }
+    public void setTitulo(String titulo){
+        this.titulo= titulo;
+    }
+    public String getAutor(){
+        return autor;
+    }
+    public void setAutor(String autor){
+        this.autor = autor;
+    }
+    public int getQuantPaginas(){
+        return quantPaginas;
+    }
+    public void setQuantPaginas(int quantPaginas){
+        this.quantPaginas = quantPaginas;
+    }
+    public double getPreco(){
+        return preco;
+    }
+    public void setPreco(double preco){
+        this.preco = preco;
+    }
+    @Override
+    public String toString(){
+        return "O livro do titulo: " +titulo+  "do autor"  +autor+ ",  que contem " +quantPaginas+   "paginas, do preço R$"+preco ;
+    }
+    @Override
+    public boolean equals (Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Livro outro = (Livro) obj;
+        return titulo.equals(outro.titulo) && autor.equals(outro.autor);
+
+    }
+    @Override
+    public int hashCode(){
+        return Objects.hash(titulo, autor);
+    }
+
 
 }
